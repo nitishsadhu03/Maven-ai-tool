abef87c49e57cb37b065f8e7d9e852013cca2178
Nitish Sadhu <nitishsadhu03@gmail.com>
2024-01-14 13:00:10 +0530

    First commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,13 @@
+import { LandingHero } from "@/components/LandingHero";
+import { LandingNavbar } from "@/components/LandingNavbar";
+
+const LandingPage = () => {
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+    </div>
+  );
+};
+
+export default LandingPage;
